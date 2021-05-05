deedf83d39b8c2431c4433ea956ac8fe56924026
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2021-05-05 16:53:38 +0100

    CPS-562: Add reporting

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,14 @@ const config: Config.InitialOptions = {
   testMatch: ['**/tests/**/(*.)spec.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
-  }
+  },
+  reporters: [
+    'default',
+    ['./node_modules/jest-html-reporter', {
+      pageTitle: 'Cases Product Suite E2E Test Report',
+      includeFailureMsg: true
+    }]
+  ]
 };
 
 export default config;
